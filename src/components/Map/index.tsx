607b4a900f885c8d2c16afefdcb86fa4c7807306
cesarzxk <43748428+cesarzxk@users.noreply.github.com>
2022-04-29 18:25:28 -0300

    Implemented: Map area select, remove and update.
    It is now possible to select an area by activating the polygon icon on the map and double clicking on the map to create a vertex
    
    It is possible to remove the vertices from the created polygon by selecting the vertex and using the trash icon.
    
    The vertices can be thought by clicking and dragging.

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,23 +1,26 @@
 import { VStack } from "@chakra-ui/react";
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, useMapEvents } from "react-leaflet";
+import MapMarker from "../MapMarkers";
 import styles from "./Map.module.css";
 
 export default function Map() {
-  const position = [51.505, -0.09];
   return (
     <VStack>
       <MapContainer
         center={{ lat: 55, lng: 10 }}
         className={styles.map}
         zoom={1}
-        scrollWheelZoom={false}
+        doubleClickZoom={false}
+        zoomControl={true}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <MapMarker />
       </MapContainer>
     </VStack>
   );
---
+++ src/components/MapMarkers/index.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useState, useRef, useMemo } from "react";
+import {
+  useMapEvents,
+  Marker,
+  Polygon,
+  Popup,
+  LayersControl,
+  LayerGroup,
+} from "react-leaflet";
+
+import { Icon, Point } from "leaflet";
+import { Box, Button } from "@chakra-ui/react";
+import { FaDrawPolygon, FaRegTrashAlt } from "react-icons/fa";
+
+type markerType = {
+  lat: number;
+  lng: number;
+};
+
+export default function MapMarker() {
+  const [markers, setMarkers] = useState<markerType[]>([]);
+  const [enabledMarkers, setEnabledMarkers] = useState(false);
+  const [markerSelected, setMarkerSelected] = useState<number>();
+
+  const [polygonMarkers, setPolygonMarkers] = useState<
+    { lat: number; lng: number }[]
+  >([]);
+
+  useEffect(() => {
+    markers.length > 0 && setPolygonMarkers([...markers, markers[0]]);
+    markers.length == 0 && setPolygonMarkers([]);
+  }, [markers]);
+
+  useMapEvents({
+    mouseout() {
+      setMarkerSelected(undefined);
+    },
+    dblclick(event) {
+      enabledMarkers && setMarkers([event.latlng, ...markers]);
+    },
+  });
+
+  function removeMarker() {
+    let newMarkes = markers.slice();
+
+    newMarkes = markers.filter((item, index) => markerSelected !== index);
+
+    setMarkerSelected(undefined);
+    setMarkers(newMarkes);
+  }
+
+  const blueMarker = new Icon({
+    iconUrl: "/marker.svg",
+    iconRetinaUrl: "/marker.svg",
+    iconSize: new Point(10, 10),
+  });
+
+  const redMarker = new Icon({
+    iconUrl: "/redmarker.svg",
+    iconRetinaUrl: "/marker.svg",
+    iconSize: new Point(10, 10),
+  });
+
+  const NewMarker = ({
+    marker,
+    index,
+  }: {
+    marker: markerType;
+    index: number;
+  }) => {
+    const markerRef = useRef<any>();
+    const eventHandlers = useMemo(
+      () => ({
+        dragend() {
+          let newMarkes = markers.slice();
+          newMarkes[index] = markerRef.current.getLatLng();
+          setMarkers(newMarkes);
+        },
+        click() {
+          setMarkerSelected(index);
+        },
+      }),
+      []
+    );
+
+    return (
+      <Marker
+        ref={markerRef}
+        eventHandlers={eventHandlers}
+        draggable
+        position={marker}
+        icon={index == markerSelected ? redMarker : blueMarker}
+      />
+    );
+  };
+
+  return (
+    <>
+      <Box marginRight="10px" className="leaflet-top leaflet-right">
+        <Box borderRadius={25} className="leaflet-control leaflet-bar">
+          <Button
+            h="3rem"
+            w="3rem"
+            borderRadius={25}
+            colorScheme={enabledMarkers ? "blue" : "gray"}
+            onClick={() => {
+              setEnabledMarkers(!enabledMarkers);
+            }}
+          >
+            <FaDrawPolygon size={26} />
+          </Button>
+        </Box>
+
+        <Box borderRadius={25} className="leaflet-control leaflet-bar">
+          <Button
+            h="3rem"
+            w="3rem"
+            disabled={markerSelected == undefined}
+            borderRadius={25}
+            colorScheme={markerSelected != undefined ? "red" : "gray"}
+            onClick={() => {
+              removeMarker();
+            }}
+          >
+            <FaRegTrashAlt size={25} />
+          </Button>
+        </Box>
+      </Box>
+
+      {markers?.map((marker, index) => (
+        <NewMarker key={index} index={index} marker={marker} />
+      ))}
+
+      {markers && <Polygon positions={polygonMarkers} />}
+    </>
+  );
+}
