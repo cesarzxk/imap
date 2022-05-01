deba4a211932b2b60e4c127ac178709240211f98
cesarzxk <43748428+cesarzxk@users.noreply.github.com>
2022-05-01 00:15:49 -0300

    Implement: Info Button.
    Have been implemented a button with a brief introduction and about the project.

---
+++ src/components/Info/index.tsx
@@ -0,0 +1,60 @@
+import {
+  Button,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  IconButton,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+
+import { GrCircleInformation } from "react-icons/gr";
+
+export default function Info() {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <IconButton
+          borderRadius="25"
+          w="3rem"
+          h="3rem"
+          aria-label=""
+          icon={<GrCircleInformation size={25} />}
+        />
+      </PopoverTrigger>
+
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader fontWeight="bold">Bem-vindo!</PopoverHeader>
+        <PopoverBody>
+          <Text>
+            Esse projeto faz parte de um desafio proposto pela empresa{" "}
+            <Link fontWeight="bold" href="https://isket.com.br/">
+              Isket
+            </Link>
+            , fique a vontade para testar!
+          </Text>
+        </PopoverBody>
+        <PopoverHeader fontWeight="bold">Instrução</PopoverHeader>
+        <PopoverBody>
+          <Text>
+            Ao ativar a ferramenta de seleção de polígono, realize 2
+            clickes/toques no mapa para criar um novo vértice, após adicionar 3
+            vértices, surgirá um quadrado em cada aresta, ao clicar em um dos
+            quadrados, um novo vértice será adicionadas o substituindo.
+          </Text>
+          <br />
+          <Text>
+            Para excluir basta selecionar o vértice que deseja excluir e em
+            seguida clickar no ícone de lixeira.
+          </Text>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+}
